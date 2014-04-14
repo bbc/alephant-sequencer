3e8e3965f7ea60cfa734ec6913b3a583668c6be8
Robert Kenny <kenoir@gmail.com>
2014-04-14 17:17:25 +0100

    fix top-level interface to sequencer

--- lib/alephant/sequencer.rb
+++ lib/alephant/sequencer.rb
@@ -6,9 +6,9 @@ module Alephant
   module Sequencer
     @@sequence_tables = {}
 
-    def self.create(table_name, ident, jsonpath = nil)
+    def self.create(table_name, ident, jsonpath = nil, keep_all = true)
       @@sequence_tables[table_name] ||= SequenceTable.new(table_name)
-      Sequencer.new(@@sequence_tables[table_name], ident, jsonpath)
+      Sequencer.new(@@sequence_tables[table_name], ident, jsonpath, keep_all)
     end
 
   end
